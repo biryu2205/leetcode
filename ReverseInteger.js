b6e996a003bf6e4c1288eb31f8f36077c66c4917
Thien Nguyen Thai 2205 <Crowclow_ZZOS@yahoo.com>
2019-10-30 11:46:51 -0700

    Upload ReverseInteger exercise Leetcode.com

---
+++ ReverseInteger.js
@@ -0,0 +1,12 @@
+Reverse = function(number) {
+ var reversed = 0;
+
+ while (number != 0) {
+  reversed *= 10;
+  reversed += number % 10;
+  number -= number % 10;
+  number /= 10;
+ }
+
+ return reversed;
+}
